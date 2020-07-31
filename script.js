4794bce136032e3cdad61e86ceca8a3c8a196395
FaudzanAdzima <faudzanad@gmail.com>
2020-07-31 15:34:57 +0700

    final-project-phase-0

---
+++ script.js
@@ -0,0 +1,19 @@
+// let nama = ''
+// let klik = document.getElementById('main')
+// let input = document.getElementById('name')
+
+// input.addEventListener('keyup', function () {
+//     // console.log(event.target.value)
+//     nama = event.target.value
+//     console.log(nama)
+//   })
+
+//   klik.addEventListener('click', function () {
+//     console.log('Ketekan mas dengan nama', nama)
+//   })
+
+  
+// prompt('Nama Lengkap')
+// prompt('Ukuran Sepatu')
+// prompt('Merk Yang Diinginkan')
+// prompt('Alamat Pengiriman')
